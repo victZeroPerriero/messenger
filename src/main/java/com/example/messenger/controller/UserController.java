4855b04edaa93f74ac4998090ceff836b9ff31a5
victor <victzeran@mail.ru>
2023-04-30 00:13:58 +0300

    add flywaymigration

--- src/main/java/com/example/messenger/controller/UserController.java
+++ src/main/java/com/example/messenger/controller/UserController.java
@@ -2,51 +2,61 @@ package com.example.messenger.controller;
 
 import com.example.messenger.model.Role;
 import com.example.messenger.model.User;
-import com.example.messenger.repo.UserRepository;
+import com.example.messenger.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
 @PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Map<String, Object> map){
-        map.put("users", userRepository.findAll());
+        map.put("users", userService.findAll());
         return "userList";
     }
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String addEditForm(@PathVariable User user, Map<String, Object> model){
         model.put("user", user);
         model.put("roles", Role.values());
         return "userEdit";
     }
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(@RequestParam String username,
             @RequestParam Map<String, String> form,
                            @RequestParam("userId") User user){
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
-        user.getRoles().clear();
-        for (String key: form.keySet()) {
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepository.save(user);
+        userService.saveUser(user,username,form);
         return "redirect:/user";
+    }
+    @GetMapping("profile")
+    public String getProfile(Map<String,Object> model,
+                             @AuthenticationPrincipal User user){
+        model.put("username", user.getUsername());
+        model.put("email", user.getMail());
+        return "profile";
+
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ){
+        userService.updateProfile(user, password, email);
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/com/example/messenger/service/UserService.java
+++ src/main/java/com/example/messenger/service/UserService.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,14 +31,7 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         user.setRoles(Collections.singleton(Role.USER));
         userRepository.save(user);
-        if(!StringUtils.isEmpty(user.getMail())){
-            String message = String.format(
-              "Hello, %s! \n" +
-              "Welcome to Messenger. Please, visit next link http://localhost:8088/activate/%s", user.getUsername(),
-                    user.getActivationCode()
-            );
-            mailSender.send(user.getMail(), "Activation code", message);
-        }
+//        sendMessage(user);
         return true;
     }
 
@@ -50,5 +43,52 @@ public class UserService implements UserDetailsService {
      user.setActivationCode(null);
      userRepository.save(user);
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key: form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+          String userEmail = user.getMail();
+
+          boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                  (userEmail != null && !userEmail.equals(email));
+          if (isEmailChanged){
+              user.setMail(email);
+              if (!StringUtils.isEmpty(email)){
+                  user.setActivationCode(UUID.randomUUID().toString());
+              }
+          }
+          if (!StringUtils.isEmpty(password)){
+              user.setPassword(password);
+          }
+          userRepository.save(user);
+          if (isEmailChanged){
+              sendMessage(user);
+          }
+    }
+
+    private void sendMessage(User user) {
+        if(!StringUtils.isEmpty(user.getMail())){
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Messenger. Please, visit next link http://localhost:8088/activate/%s", user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getMail(), "Activation code", message);
+        }
     }
 }
